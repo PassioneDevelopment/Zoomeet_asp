72c0ce9303cb4f8fe5c4adaabaecde61ab9ceb2b
José Aguiar <zeca_henrique22@hotmail.com>
2022-06-30 21:15:11 -0300

    Project Creation
    project cration.

---
+++ Zoomeet/Zoomeet.Site/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Zoomeet.Site.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
